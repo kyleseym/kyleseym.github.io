bed0e07ec0f21e26917b6c22a8c666cddc36f886
Kyle Seymour <kyleseym@gmail.com>
2020-09-20 05:32:18 -0500

    cant run this on github

--- W50Final/W50Final/W50Final/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-Package for W50Final.
-"""
--- W50Final/W50Final/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\kyles\source\repos\W50Final\W50Final\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
